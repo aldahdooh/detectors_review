6f73167f7e4fb4d660b0b49d8d518397075ec0d4
aldahdooh <62994061+aldahdooh@users.noreply.github.com>
2021-02-24 14:28:52 +0100

    Add files via upload

---
+++ kd_bu/detect/util.py
@@ -0,0 +1,446 @@
+from __future__ import division, absolute_import, print_function
+
+import os
+import multiprocessing as mp
+from subprocess import call
+import warnings
+import numpy as np
+import scipy.io as sio
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.preprocessing import scale, StandardScaler
+import keras.backend as K
+from keras.datasets import mnist, cifar10
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.regularizers import l2
+
+# Gaussian noise scale sizes that were determined so that the average
+# L-2 perturbation size is equal to that of the adversarial samples
+STDEVS = {
+    'mnist': {'fgsm_0.03125': 0.03125, 'fgsm_0.0625': 0.0625, 'fgsm_0.125': 0.125, 'fgsm_0.25': 0.25, 'fgsm_0.3125': 0.3125, 'fgsm_0.5': 0.5,\
+            'bim_0.03125': 0.03125, 'bim_0.0625': 0.0625, 'bim_0.125': 0.125, 'bim_0.25': 0.25, 'bim_0.3125': 0.3125, 'bim_0.5': 0.5,\
+            'pgd1_5': 0.03125, 'pgd1_10': 0.0625, 'pgd1_15': 0.125, 'pgd1_20': 0.125, 'pgd1_25': 0.125, 'pgd1_30': 0.25, 'pgd1_40': 0.3125,\
+            'pgd2_0.25': 0.03125, 'pgd2_0.3125': 0.0625, 'pgd2_0.5': 0.125, 'pgd2_1': 0.125, 'pgd2_1.5': 0.25, 'pgd2_2': 0.3125,\
+            'pgdi_0.03125': 0.03125, 'pgdi_0.0625': 0.0625, 'pgdi_0.125': 0.125, 'pgdi_0.25': 0.25, 'pgdi_0.3125': 0.3125, 'pgdi_0.5': 0.5,\
+            'cwi': 0.25, 'df': 0.25,\
+            'hca_0.03125': 0.03125, 'hca_0.0625': 0.0625, 'hca_0.125': 0.125, 'hca_0.25': 0.25, 'hca_0.3125': 0.3125, 'hca_0.5': 0.5,\
+            'sa': 0.3, 'sta': 0.3, 'hop': 0.3
+            },
+    'cifar': {'fgsm_0.03125': 0.03125, 'fgsm_0.0625': 0.0625, 'fgsm_0.125': 0.125, 'fgsm_0.25': 0.25, 'fgsm_0.3125': 0.3125, 'fgsm_0.5': 0.5,\
+            'bim_0.03125': 0.03125, 'bim_0.0625': 0.0625, 'bim_0.125': 0.125, 'bim_0.25': 0.25, 'bim_0.3125': 0.3125, 'bim_0.5': 0.5,\
+            'pgd1_5': 0.03125, 'pgd1_10': 0.0625, 'pgd1_15': 0.125, 'pgd1_20': 0.125, 'pgd1_25': 0.125, 'pgd1_30': 0.25, 'pgd1_40': 0.3125,\
+            'pgd2_0.25': 0.03125, 'pgd2_0.3125': 0.0625, 'pgd2_0.5': 0.125, 'pgd2_1': 0.125, 'pgd2_1.5': 0.25, 'pgd2_2': 0.3125,\
+            'pgdi_0.03125': 0.03125, 'pgdi_0.0625': 0.0625, 'pgdi_0.125': 0.125, 'pgdi_0.25': 0.25, 'pgdi_0.3125': 0.3125, 'pgdi_0.5': 0.5,\
+            'cwi': 0.125, 'df': 0.125,\
+            'hca_0.03125': 0.03125, 'hca_0.0625': 0.0625, 'hca_0.125': 0.125, 'hca_0.25': 0.25, 'hca_0.3125': 0.3125, 'hca_0.5': 0.5,\
+            'sa': 0.125, 'sta': 0.125, 'hop': 0.125
+            },
+    'svhn': {'fgsm_0.03125': 0.03125, 'fgsm_0.0625': 0.0625, 'fgsm_0.125': 0.125, 'fgsm_0.25': 0.25, 'fgsm_0.3125': 0.3125, 'fgsm_0.5': 0.5,\
+            'bim_0.03125': 0.03125, 'bim_0.0625': 0.0625, 'bim_0.125': 0.125, 'bim_0.25': 0.25, 'bim_0.3125': 0.3125, 'bim_0.5': 0.5,\
+            'pgd1_5': 0.03125, 'pgd1_10': 0.0625, 'pgd1_15': 0.125, 'pgd1_20': 0.125, 'pgd1_25': 0.125, 'pgd1_30': 0.25, 'pgd1_40': 0.3125,\
+            'pgd2_0.25': 0.03125, 'pgd2_0.3125': 0.0625, 'pgd2_0.5': 0.125, 'pgd2_1': 0.125, 'pgd2_1.5': 0.25, 'pgd2_2': 0.3125,\
+            'pgdi_0.03125': 0.03125, 'pgdi_0.0625': 0.0625, 'pgdi_0.125': 0.125, 'pgdi_0.25': 0.25, 'pgdi_0.3125': 0.3125, 'pgdi_0.5': 0.5,\
+            'cwi': 0.125, 'df': 0.125,\
+            'hca_0.03125': 0.03125, 'hca_0.0625': 0.0625, 'hca_0.125': 0.125, 'hca_0.25': 0.25, 'hca_0.3125': 0.3125, 'hca_0.5': 0.5,\
+            'sa': 0.125, 'sta': 0.125, 'hop': 0.125
+            },
+    'tiny': {'fgsm_0.03125': 0.03125, 'fgsm_0.0625': 0.0625, 'fgsm_0.125': 0.125, 'fgsm_0.25': 0.25, 'fgsm_0.3125': 0.3125, 'fgsm_0.5': 0.5,\
+            'bim_0.03125': 0.03125, 'bim_0.0625': 0.0625, 'bim_0.125': 0.125, 'bim_0.25': 0.25, 'bim_0.3125': 0.3125, 'bim_0.5': 0.5,\
+            'pgd1_5': 0.03125, 'pgd1_10': 0.0625, 'pgd1_15': 0.125, 'pgd1_20': 0.125, 'pgd1_25': 0.125, 'pgd1_30': 0.25, 'pgd1_40': 0.3125,\
+            'pgd2_0.25': 0.03125, 'pgd2_0.3125': 0.0625, 'pgd2_0.5': 0.125, 'pgd2_1': 0.125, 'pgd2_1.5': 0.25, 'pgd2_2': 0.3125,\
+            'pgdi_0.03125': 0.03125, 'pgdi_0.0625': 0.0625, 'pgdi_0.125': 0.125, 'pgdi_0.25': 0.25, 'pgdi_0.3125': 0.3125, 'pgdi_0.5': 0.5,\
+            'cwi': 0.125, 'df': 0.125,\
+            'hca_0.03125': 0.03125, 'hca_0.0625': 0.0625, 'hca_0.125': 0.125, 'hca_0.25': 0.25, 'hca_0.3125': 0.3125, 'hca_0.5': 0.5,\
+            'sa': 0.125, 'sta': 0.125, 'hop': 0.125
+            },
+}
+# Set random seed
+np.random.seed(0)
+
+
+def get_data(dataset='mnist'):
+    """
+    TODO
+    :param dataset:
+    :return:
+    """
+    assert dataset in ['mnist', 'cifar', 'svhn'], \
+        "dataset parameter must be either 'mnist' 'cifar' or 'svhn'"
+    if dataset == 'mnist':
+        # the data, shuffled and split between train and test sets
+        (X_train, y_train), (X_test, y_test) = mnist.load_data()
+        # reshape to (n_samples, 28, 28, 1)
+        X_train = X_train.reshape(-1, 28, 28, 1)
+        X_test = X_test.reshape(-1, 28, 28, 1)
+    elif dataset == 'cifar':
+        # the data, shuffled and split between train and test sets
+        (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    else:
+        if not os.path.isfile("../data/svhn_train.mat"):
+            print('Downloading SVHN train set...')
+            call(
+                "curl -o ../data/svhn_train.mat "
+                "http://ufldl.stanford.edu/housenumbers/train_32x32.mat",
+                shell=True
+            )
+        if not os.path.isfile("../data/svhn_test.mat"):
+            print('Downloading SVHN test set...')
+            call(
+                "curl -o ../data/svhn_test.mat "
+                "http://ufldl.stanford.edu/housenumbers/test_32x32.mat",
+                shell=True
+            )
+        train = sio.loadmat('../data/svhn_train.mat')
+        test = sio.loadmat('../data/svhn_test.mat')
+        X_train = np.transpose(train['X'], axes=[3, 0, 1, 2])
+        X_test = np.transpose(test['X'], axes=[3, 0, 1, 2])
+        # reshape (n_samples, 1) to (n_samples,) and change 1-index
+        # to 0-index
+        y_train = np.reshape(train['y'], (-1,)) - 1
+        y_test = np.reshape(test['y'], (-1,)) - 1
+
+    # cast pixels to floats, normalize to [0, 1] range
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train /= 255
+    X_test /= 255
+
+    # one-hot-encode the labels
+    Y_train = np_utils.to_categorical(y_train, 10)
+    Y_test = np_utils.to_categorical(y_test, 10)
+
+    print(X_train.shape)
+    print(Y_train.shape)
+    print(X_test.shape)
+    print(Y_test.shape)
+
+    return X_train, Y_train, X_test, Y_test
+
+
+def get_model(dataset='mnist'):
+    """
+    Takes in a parameter indicating which model type to use ('mnist',
+    'cifar' or 'svhn') and returns the appropriate Keras model.
+    :param dataset: A string indicating which dataset we are building
+                    a model for.
+    :return: The model; a Keras 'Sequential' instance.
+    """
+    assert dataset in ['mnist', 'cifar', 'svhn'], \
+        "dataset parameter must be either 'mnist' 'cifar' or 'svhn'"
+    if dataset == 'mnist':
+        # MNIST model
+        layers = [
+            Conv2D(64, (3, 3), padding='valid', input_shape=(28, 28, 1)),
+            Activation('relu'),
+            Conv2D(64, (3, 3)),
+            Activation('relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Dropout(0.5),
+            Flatten(),
+            Dense(128),
+            Activation('relu'),
+            Dropout(0.5),
+            Dense(10),
+            Activation('softmax')
+        ]
+    elif dataset == 'cifar':
+        # CIFAR-10 model
+        layers = [
+            Conv2D(32, (3, 3), padding='same', input_shape=(32, 32, 3)),
+            Activation('relu'),
+            Conv2D(32, (3, 3), padding='same'),
+            Activation('relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Conv2D(64, (3, 3), padding='same'),
+            Activation('relu'),
+            Conv2D(64, (3, 3), padding='same'),
+            Activation('relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Conv2D(128, (3, 3), padding='same'),
+            Activation('relu'),
+            Conv2D(128, (3, 3), padding='same'),
+            Activation('relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Flatten(),
+            Dropout(0.5),
+            Dense(1024, kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01)),
+            Activation('relu'),
+            Dropout(0.5),
+            Dense(512, kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01)),
+            Activation('relu'),
+            Dropout(0.5),
+            Dense(10),
+            Activation('softmax')
+        ]
+    else:
+        # SVHN model
+        layers = [
+            Conv2D(64, (3, 3), padding='valid', input_shape=(32, 32, 3)),
+            Activation('relu'),
+            Conv2D(64, (3, 3)),
+            Activation('relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Dropout(0.5),
+            Flatten(),
+            Dense(512),
+            Activation('relu'),
+            Dropout(0.5),
+            Dense(128),
+            Activation('relu'),
+            Dropout(0.5),
+            Dense(10),
+            Activation('softmax')
+        ]
+
+    model = Sequential()
+    for layer in layers:
+        model.add(layer)
+
+    return model
+
+
+def flip(x, nb_diff):
+    """
+    Helper function for get_noisy_samples
+    :param x:
+    :param nb_diff:
+    :return:
+    """
+    original_shape = x.shape
+    x = np.copy(np.reshape(x, (-1,)))
+    candidate_inds = np.where(x < 0.99)[0]
+    #assert candidate_inds.shape[0] >= nb_diff
+    inds = np.random.choice(candidate_inds, nb_diff)
+    x[inds] = 1.
+
+    return np.reshape(x, original_shape)
+
+
+def get_noisy_samples(X_test, X_test_adv, dataset, attack):
+    """
+    TODO
+    :param X_test:
+    :param X_test_adv:
+    :param dataset:
+    :param attack:
+    :return:
+    """
+    # if attack.startswith('fgsm') or attack.startswith('pgd') or attack.startswith('hca') or attack.startswith('bim'):
+    #     # warnings.warn("Using pre-set Gaussian scale sizes to craft noisy "
+    #     #               "samples. If you've altered the eps/eps-iter parameters "
+    #     #               "of the attacks used, you'll need to update these. In "
+    #     #               "the future, scale sizes will be inferred automatically "
+    #     #               "from the adversarial samples.")
+    #     # Add Gaussian noise to the samples
+    #     X_test_noisy = np.minimum(
+    #         np.maximum(
+    #             X_test + np.random.normal(loc=0, scale=STDEVS[dataset][attack],
+    #                                       size=X_test.shape),
+    #             0
+    #         ),
+    #         1
+    #     )
+    # else:
+    #     X_test_noisy = np.zeros_like(X_test)
+    #     for i in range(len(X_test)):
+    #         # Count the number of pixels that are different
+    #         nb_diff = len(np.where(X_test[i] != X_test_adv[i])[0])
+    #         # Randomly flip an equal number of pixels (flip means move to max
+    #         # value of 1)
+    #         X_test_noisy[i] = flip(X_test[i], nb_diff)
+
+    X_test_noisy = np.minimum(
+            np.maximum(
+                X_test + np.random.normal(loc=0, scale=STDEVS[dataset][attack],
+                                          size=X_test.shape),
+                0
+            ),
+            1
+        )
+
+    return X_test_noisy
+
+
+def get_mc_predictions(model, X, nb_iter=50, batch_size=256):
+    """
+    TODO
+    :param model:
+    :param X:
+    :param nb_iter:
+    :param batch_size:
+    :return:
+    """
+    output_dim = model.layers[-1].output.shape[-1]
+    get_output = K.function(
+        [model.layers[0].input, K.learning_phase()],
+        [model.layers[-1].output]
+    )
+
+    def predict():
+        n_batches = int(np.ceil(X.shape[0] / float(batch_size)))
+        output = np.zeros(shape=(len(X), output_dim))
+        for i in range(n_batches):
+            output[i * batch_size:(i + 1) * batch_size] = \
+                get_output([X[i * batch_size:(i + 1) * batch_size], 1])[0]
+        return output
+
+    preds_mc = []
+    for i in tqdm(range(nb_iter)):
+        preds_mc.append(predict())
+
+    return np.asarray(preds_mc)
+
+
+def get_deep_representations(model, X, batch_size=256, dataset='mnist'):
+    """
+    TODO
+    :param model:
+    :param X:
+    :param batch_size:
+    :return:
+    """
+    if dataset=='tiny' or dataset=='cifar':
+        # last hidden layer is always at index -4
+        output_dim = model.layers[-3].output.shape[-1]
+        get_encoding = K.function(
+            [model.layers[0].input, K.learning_phase()],
+            [model.layers[-3].output]
+        )
+    else:
+        # last hidden layer is always at index -4
+        output_dim = model.layers[-4].output.shape[-1]
+        get_encoding = K.function(
+            [model.layers[0].input, K.learning_phase()],
+            [model.layers[-4].output]
+        )
+
+    n_batches = int(np.ceil(X.shape[0] / float(batch_size)))
+    output = np.zeros(shape=(len(X), output_dim))
+    for i in range(n_batches):
+        output[i * batch_size:(i + 1) * batch_size] = \
+            get_encoding([X[i * batch_size:(i + 1) * batch_size], 0])[0]
+
+    return output
+
+
+def score_point(tup):
+    """
+    TODO
+    :param tup:
+    :return:
+    """
+    x, kde = tup
+
+    return kde.score_samples(np.reshape(x, (1, -1)))[0]
+
+
+def score_samples(kdes, samples, preds, n_jobs=None):
+    """
+    TODO
+    :param kdes:
+    :param samples:
+    :param preds:
+    :param n_jobs:
+    :return:
+    """
+    if n_jobs is not None:
+        p = mp.Pool(n_jobs)
+    else:
+        p = mp.Pool()
+    results = np.asarray(
+        p.map(
+            score_point,
+            [(x, kdes[i]) for x, i in zip(samples, preds)]
+        )
+    )
+    p.close()
+    p.join()
+
+    return results
+
+
+def normalize(normal, adv, noisy):
+    """
+    TODO
+    :param normal:
+    :param adv:
+    :param noisy:
+    :return:
+    """
+    n_samples = len(normal)
+    total = scale(np.concatenate((normal, adv, noisy)))
+
+    return total[:n_samples], total[n_samples:2*n_samples], total[2*n_samples:]
+
+def normalize_std(normal, adv, noisy):
+    """
+    TODO
+    :param normal:
+    :param adv:
+    :param noisy:
+    :return:
+    """
+    n_samples = len(normal)
+    scaler = StandardScaler()
+    total = scaler.fit_transform(np.concatenate((normal, adv, noisy)).reshape((-1,1)))
+    total = total.reshape((-1,))
+    # total = scale(np.concatenate((normal, adv, noisy)))
+
+    return total[:n_samples], total[n_samples:2*n_samples], total[2*n_samples:], scaler
+
+
+def train_lr(densities_pos, densities_neg, uncerts_pos, uncerts_neg):
+    """
+    TODO
+    :param densities_pos:
+    :param densities_neg:
+    :param uncerts_pos:
+    :param uncerts_neg:
+    :return:
+    """
+    values_neg = np.concatenate(
+        (densities_neg.reshape((1, -1)),
+         uncerts_neg.reshape((1, -1))),
+        axis=0).transpose([1, 0])
+    values_pos = np.concatenate(
+        (densities_pos.reshape((1, -1)),
+         uncerts_pos.reshape((1, -1))),
+        axis=0).transpose([1, 0])
+
+    values = np.concatenate((values_neg, values_pos))
+    labels = np.concatenate(
+        (np.zeros_like(densities_neg), np.ones_like(densities_pos)))
+
+    lr = LogisticRegressionCV(n_jobs=-1, max_iter=1000).fit(values, labels)
+
+    return values, labels, lr
+
+
+def compute_roc(probs_neg, probs_pos, plot=False):
+    """
+    TODO
+    :param probs_neg:
+    :param probs_pos:
+    :param plot:
+    :return:
+    """
+    probs = np.concatenate((probs_neg, probs_pos))
+    labels = np.concatenate((np.zeros_like(probs_neg), np.ones_like(probs_pos)))
+    fpr, tpr, _ = roc_curve(labels, probs)
+    auc_score = auc(fpr, tpr)
+    if plot:
+        plt.figure(figsize=(7, 6))
+        plt.plot(fpr, tpr, color='blue',
+                 label='ROC (AUC = %0.4f)' % auc_score)
+        plt.legend(loc='lower right')
+        plt.title("ROC Curve")
+        plt.xlabel("FPR")
+        plt.ylabel("TPR")
+        plt.show()
+
+    return fpr, tpr, auc_score
